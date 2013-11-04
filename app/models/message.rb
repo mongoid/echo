40bafb24d449c8da5913a37caa9b94175981fc22
Arthur Neves <arthurnn@gmail.com>
2013-11-03 22:39:08 -0500

    Better fk on message_reports

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,5 +2,5 @@ class Message
   include Mongoid::Document
 
   field :message_id, :default => proc { "fail-#{Time.now.to_f.to_s}" }
-  has_many :message_reports, primary_key: :message_id, foreign_key: :message_id
+  has_many :message_reports, primary_key: :message_id, foreign_key: :message_report_id
 end
--- app/models/message_report.rb
+++ app/models/message_report.rb
@@ -1,9 +1,6 @@
 class MessageReport
   include Mongoid::Document
 
-
-  field :message_id, :default => proc { "fail-#{Time.now.to_f.to_s}" }
-
-  belongs_to :message, primary_key: :message_id, foreign_key: :message_id
-
+  field :message_report_id, :default => proc { "fail-#{Time.now.to_f.to_s}" }
+  belongs_to :message, primary_key: :message_id, foreign_key: :message_report_id
 end
